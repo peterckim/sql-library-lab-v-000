f4e4fb5b6f33be489fef7b79e27d4495e16c0d31
Peter Kim <peterckim96@gmail.com>
2018-07-02 23:47:14 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, COUNT(characters.species), characters.motto FROM characters INNER JOIN series ON characters.series_id = series.id GROUP BY characters.series_id, characters.species;"
+  "SELECT series.title, COUNT(characters.species) FROM characters INNER JOIN series ON characters.series_id = series.id GROUP BY characters.series_id, characters.species;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
